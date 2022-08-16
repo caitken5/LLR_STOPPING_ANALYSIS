Added functions for filtering velocity data and deleting data from a list without running into issues of changing index. Fixed the code to calculate and graph when stopping occurs in a particular signal. Printed off graphs!

main.py
@@ -6,14 +6,18 @@ import gc
 import header as h
 
 
+source_folder = "D:/PD_Participant_Data/LLR_DATA_ANALYSIS_CLEANED/LLR_DATA_PROCESSING_PIPELINE/" \
+                "4_LLR_DATA_SEGMENTATION/NPZ_FILES_BY_TARGET"
+storage_name = "D:/PD_Participant_Data/LLR_DATA_ANALYSIS_CLEANED/LLR_DATA_PROCESSING_PIPELINE/7_LLR_STOPPING_GRAPHS/" \
+               "STOPPING_REGIONS"
 
-source_folder = "D:/PD_Participant_Data/LLR_DATA_ANALYSIS_CLEANED/LLR_DATA_PROCESSING_PIPELINE/4_LLR_DATA_SEGMENTATION/" \
-                "NPZ_FILES_BY_TARGET"
-storage_name = "D:/PD_Participant_Data/LLR_DATA_ANALYSIS_CLEANED/LLR_DATA_PROCESSING_PIPELINE/7_LLR_STOPPING_GRAPHS"
-
-testing = True
-vel_limit = 0.0025  # Chosen to account for minor noise in signal but also to select regions of data in which a user
-# may have paused during a movement.
+testing = False
+# TODO: Play with vel_limit and stop_limit to confirm these are good indicators of a person stopping movement.
+#  I think the threshold is a little low right now.
+vel_limit = 0.00025  # Chosen to account for minor noise in signal but also to select regions of data in which a user
+# may have stopped during a movement.
+stop_limit = 10  # The least number of acceptable absolute values below vel_limit in the time series "dd" that
+# represents a stop. Used to exclude regions where one slows to immediately start another motion.
 
 if __name__ == '__main__':
     print("Running frequency analysis...")
@@ -33,45 +37,53 @@ if __name__ == '__main__':
                 for i in range(len(ragged_list)):
                     stuff = ragged_list[i]
                     t = stuff[:, h.data_header.index("Time")]
+                    v_unfilt = stuff[:, h.data_header.index("Vxy_Mag")]
+                    v = h.butterworth_filter(v_unfilt)
+                    dv = np.gradient(v)
                     d = stuff[:, h.data_header.index("Dist_From_Target")]
-                    dd = np.gradient(d)
-                    ddd = np.gradient(dd)
-                    v = stuff[:, h.data_header.index("Vxy_Mag")]
-                    vx = stuff[:, h.data_header.index("X_Vel")]
-                    vy = stuff[:, h.data_header.index("Y_Vel")]
-
                     # Plotting of data here
                     fig = plt.figure(num=1, dpi=100, facecolor='w', edgecolor='w')
                     fig.set_size_inches(25, 8)
                     ax1 = fig.add_subplot(111)
                     ax1.grid(visible=True)
+                    ax1.plot(t, v_unfilt*100, label="Unfiltered Absolute Velocity of Robot [mm/s]*100")
                     ax1.plot(t, d/10, label="Distance from Target (DfT) [cm]")
-                    ax1.plot(t, dd*10, label="Velocity of DfT [mm/s]*10")
-                    ax1.plot(t, ddd*10, label="Acceleration of DfT [mm/s^2]*10")
+                    ax1.plot(t, dv*10, label="Acceleration of Robot [mm/s]*10")
                     ax1.plot(t, v*100, label="Absolute Velocity of Robot [mm/s]*100")
                     # Identify points of the velocity of distance from target (DfT) below selected threshold.
-                    pauses = np.asarray(np.nonzero((dd < vel_limit) & (dd > -vel_limit)))[0]  # This corresponds to the indices of elements where pause
-                    # occurs.
-                    # Return the elements of pauses where the row elements of pauses is non-sequential.
-                    pause_rows = np.diff(pauses)
-                    pause_row_ends = np.nonzero(pause_rows > 1)
-                    start = 0
-                    pause_list = []
-                    for j in pause_row_ends:
-                        # Using the number of times where the index changes, retrieve the indices in pauses that correlate to indices where a pause occurred. Then, calculate the amount of time spent in each pause state, and how many pauses occurred.
-                        val = int(j[0]) + 1
-                        temp = pauses[start:val]  # We want to ensure we sample the entire point up to the end of the pause.
-
-                        if temp.shape[0] > 10:
-                            temp = temp[:, np.newaxis]
-                            pause_list.append(temp)
-                        start = val
-                    pause_list.append(pauses[start:])
-
-                    for j in range(len(pause_list)):
-                        t1 = t[pause_list[j][0]]
-                        t2 = t[pause_list[j][-1]]
-                        ax1.axvspan(t1, t2, color='orange', alpha=0.5)
+                    # TODO: Convert the stopping criteria into a function so that it is simplified when transferred to
+                    #  LLR_DATA_ANALYSIS code.
+                    stops = np.asarray(np.nonzero((v < vel_limit) & (v > -vel_limit)))[0]  # This corresponds to the
+                    # indices of elements where a stop occurs.
+                    temp = []
+                    stop_list = []
+                    if stops.shape[0] != 0:
+                        for j, k in enumerate(stops):  # Append all the values as new arrays to the stop_list.
+                            # Does not yet deal if arrays are below a certain size.
+                            temp.append(k)
+                            if k == stops[-1]:
+                                arr = np.asarray(temp)
+                                stop_list.append(arr)
+                            elif (stops[j+1] - k) > 1:
+                                arr = np.asarray(temp)
+                                stop_list.append(arr)
+                                temp = []
+                        # As long as stops has some length, the above code will result in the stop_list containing some information.
+                        # Now remove arrays from stop_list that are not greater than number of samples specified in stop_limit.
+                        remove_list = []  # Used to append values to then remove values from stop_list all at once.
+                        for j in range(len(stop_list)):
+                            len_stop = stop_list[j].shape[0]
+                            if len_stop < stop_limit:
+                                # Add element to remove_list.
+                                remove_list.append(j)
+                        if len(remove_list) > 0:
+                            # If remove_list is empty, this below function would probably remove the final element, or some kind of error.
+                            h.delete_multiple_element(stop_list, remove_list)
+                    if len(stop_list) > 0:
+                        for j in range(len(stop_list)):
+                            t1 = t[stop_list[j][0]]
+                            t2 = t[stop_list[j][-1]]
+                            ax1.axvspan(t1, t2, color='orange', alpha=0.5)
                     # Set some labels.
                     file_name = file.split('.')[0]
                     ax1.set_xlabel("Time [s]")
@@ -87,4 +99,3 @@ if __name__ == '__main__':
                         plt.savefig(fname=save_str)
                         fig.clf()
                         gc.collect()
-