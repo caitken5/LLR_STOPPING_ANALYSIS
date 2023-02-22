42ed6fda77b61d6241f78a50889a0fd3fee0032e
sobriqu83 <caitken5@uwo.ca>
2023-02-22 12:00:04 -0500

    Changed tick sizes. Changed code to only print files from one person, which is only to speed up printing of graphs for papers and thesis. Adjusted some labels in the plotting.

--- main.py
+++ main.py
@@ -22,22 +22,22 @@ dt = 0.01  # The period between each sample obtained from the robot.
 dist_limit = 10  # This represents the number of mm in radius around the target in which a pause occurs.
 
 # Create some variables for controlling the font size.
-SMALLER_SIZE = 10
-SMALL_SIZE = 18
+SMALLER_SIZE = 18
+SMALL_SIZE = 24
 
 # Change the sizes for specific features of plots accordingly.
 plt.rc('axes', labelsize=SMALL_SIZE)
 plt.rc('legend', fontsize=SMALL_SIZE)
-plt.rc('xtick', labelsize=SMALLER_SIZE)
-plt.rc('ytick', labelsize=SMALLER_SIZE)
+plt.rc('xtick', labelsize=SMALL_SIZE)
+plt.rc('ytick', labelsize=SMALL_SIZE)
 
 
 if __name__ == '__main__':
-    print("Running frequency analysis...")
+    print("Running stopping analysis...")
     for file in os.listdir(source_folder):
         if file.endswith('.npz'):
             task_number = h.get_task_number(file)
-            if (("T1" in file) or ("T2" in file)) & ("V0" in file):
+            if (("T1" in file) or ("T2" in file)) & ("V0" in file) & ("SCS_33_V0_T1_U_L" in file):
                 # Here if file includes task 1 and 2, and if it also is a V0 file, as I'm not studying other pieces
                 # of information.
                 print(file)
@@ -60,10 +60,10 @@ if __name__ == '__main__':
                     fig.set_size_inches(15, 8)
                     ax1 = fig.add_subplot(111)
                     ax1.grid(visible=True)
-                    # ax1.plot(t, f, 'k-', label="Force [N]")
-                    ax1.plot(t, d/10, 'k-', label="Distance from Target [cm]")
-                    # ax1.plot(t, dv*10, label="Acceleration of Robot / 10 [cm/$s^2$]")
-                    ax1.plot(t, v*100, 'k:', label="Absolute Velocity [cm/s]")
+                    ax1.plot(t, f, 'k--', label="Force (N)")
+                    ax1.plot(t, d/10, 'k-', label="Distance from Target (cm)")
+                    # ax1.plot(t, dv*10, label="Acceleration of Robot / 10 (cm/$s^2$)")
+                    ax1.plot(t, v*100, 'k:', label="Absolute Velocity (cm/s)")
                     # Identify points of the velocity of distance from target (DfT) below selected threshold.
                     stop_list = h.calculate_stops(v, vel_limit, stop_limit)
                     total_time_stopped = 0
@@ -105,11 +105,11 @@ if __name__ == '__main__':
                                 ax1.axvspan(t1, t2, color='grey', alpha=0.5)
                     # Set some labels.
                     file_name = file.split('.')[0]
-                    ax1.set_xlabel("Time [s]")
+                    ax1.set_xlabel("Time (s)")
                     ax1.set_ylabel("Magnitude")
                     # ax1.set_title("Identifying Potential Stopping Points for Sample " + file_name + "_" + str(i) +
                     #              " \n Using Absolute Distance from Target, Velocity, and It's Derivatives")
-                    plt.legend()
+                    plt.legend(loc="upper right")
                     save_str = storage_name + '/' + file_name + "_" + str(i)
                     if testing & make_graphs:
                         plt.show()
